fdddb5077fe951ea7629260eb4cb86abf52af167
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2023-04-20 18:14:31 +0200

    fixing and adding 1

--- app/app.js
+++ app/app.js
@@ -4,9 +4,15 @@ const homeLi=document.querySelector('#homeLi');
 const contactLi=document.querySelector('#contactLi')
 const aboutLi=document.querySelector('#aboutLi')
 const tContainer=document.querySelector('#tContainer')
+const land=document.querySelector('#land');
+const letterName=document.querySelector('#letterName');
+const down=document.querySelector('#down');
+const burguer=document.querySelector('#burguer')
+const burguerMenu=document.querySelector('#burguerMenu')
 
 const callback = (entries) => {
     entries.forEach((entry) => {
+
       if (entry.isIntersecting) {
         header.classList.add('bg-gray-300','text-black')
         header.classList.remove('bg-gray-800','text-blue-50')
@@ -16,6 +22,9 @@ const callback = (entries) => {
         contactLi.classList.remove('hover:text-blue-600')
         aboutLi.classList.add('hover:text-white')
         aboutLi.classList.remove('hover:text-blue-600')
+        burguerMenu.classList.add('bg-gray-300','text-black')
+        burguerMenu.classList.remove('bg-gray-800','text-blue-50')
+
 
       }else{
         header.classList.add('bg-gray-800','text-blue-50')
@@ -25,26 +34,55 @@ const callback = (entries) => {
         contactLi.classList.add('hover:text-blue-600')
         contactLi.classList.remove('hover:text-white')
         aboutLi.classList.add('hover:text-blue-600')
-        aboutLi.classList.remove('hover:text-white') 
+        aboutLi.classList.remove('hover:text-white')
+        burguerMenu.classList.add('bg-gray-800','text-blue-50')
+        burguerMenu.classList.remove('bg-gray-300','text-black')
+
+
+
       }
     });
   };
+  
+  burguer.addEventListener('click',()=>{
+    if(burguer.classList.contains('open')){
+      burguer.classList.remove('open')
+      burguerMenu.classList.remove('containerBurguer')
+    }else{
+      burguer.classList.add('open')
+      burguerMenu.classList.add('containerBurguer')
+
+    }
+  })
   
   const observer = new IntersectionObserver(callback);
   
   observer.observe(hero);
 
-  const text1=`Hello world <br>
-  Im José Antonio Román Moreno a FullStack Developer. <br>
-  Nice to meet you!`
+  window.addEventListener('scroll',()=>{
+  const position=hero.getBoundingClientRect().top;
+  const height = 161.60000610351562;
+
+
+  if (position>height){
+    header.classList.remove('opacity-100');
+    header.classList.add('opacity-0');
+    
+  }else{
+    header.classList.add('opacity-100');
+    header.classList.add('top-0');
+    header.classList.remove('opacity-0');
+  }
+    
+  })
   
   const text2=`You are completely free to take a look around my website.
   <br /> <br /> And browse my social media and Github!!!`
 
-  const text3='I made this website using JS and Tailwind.'
+  const text3='I made this website using JavaScript, CSS and Tailwind.'
 
 
-  let carousel = [text1, text2, text3];
+  let carousel = [text2, text3];
 
   tContainer.innerHTML = `${carousel[0]}`;
 
@@ -54,8 +92,17 @@ const callback = (entries) => {
     setInterval(() => {
       currentIndex = currentIndex === carousel.length - 1 ? 0 : currentIndex + 1;
       tContainer.innerHTML = `${carousel[currentIndex]}`;
-    }, 4500);
+    }, 3500);
   });
   
-  
+  document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      letterName.style.border='none'
+    }, 6300);
+  });
+  down.addEventListener('click',()=>{
+    hero.scrollIntoView({
+      behavior:"smooth"
+  })
+  })
   
